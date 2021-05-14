52e539fdea9b0c1792a13896a51223ca5794833d
Nikita Schinnikov <nschinnikov@gmail.com>
2021-05-14 11:26:08 +0500

    move net config to vpc module

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -5,29 +5,22 @@ provider "yandex" {
   folder_id = var.folder_id
   zone      = var.zone
 }
-resource "yandex_vpc_network" "app-network" {
-  name = "reddit-app-network"
-}
-resource "yandex_vpc_subnet" "app-subnet" {
-  name           = "reddit-app-subnet"
-  zone           = "ru-central1-a"
-  network_id     = "${yandex_vpc_network.app-network.id}"
-  v4_cidr_blocks = ["192.168.10.0/24"]
-  depends_on     = [yandex_vpc_network.app-network]
+module "vpc" {
+  source           = "../modules/vpc"
 }
 module "app" {
   source           = "../modules/app"
   public_key_path  = var.public_key_path
   private_key_path = var.private_key_path
   app_disk_image   = var.app_disk_image
-  subnet_id        = yandex_vpc_subnet.app-subnet.id
+  subnet_id        = module.vpc.subnet_id
 }
 module "db" {
   source           = "../modules/db"
   public_key_path  = var.public_key_path
   private_key_path = var.private_key_path
   db_disk_image    = var.db_disk_image
-  subnet_id        = yandex_vpc_subnet.app-subnet.id
+  subnet_id        = module.vpc.subnet_id
 }
 
 
